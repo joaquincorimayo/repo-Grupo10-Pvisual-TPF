d5029ea1fed08f0f1c001dce1c1a10b278006816
joaquincorimayo <joaquin97corimayo@gmail.com>
2022-06-14 15:03:44 -0300

    Estructura inicial.

---
+++ TPF_Grupo10/src/main/java/ar/edu/unju/fi/tpf/TpfGrupo10Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.tpf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpfGrupo10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpfGrupo10Application.class, args);
+	}
+
+}
