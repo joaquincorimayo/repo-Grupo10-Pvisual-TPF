15099c6ea09570a793b5a52577f0cba5902578a6
joaquincorimayo <joaquin97corimayo@gmail.com>
2022-06-18 12:06:27 -0300

    Corimayo: Primera capa de presentacion.

--- TPF_Grupo10/src/main/java/ar/edu/unju/fi/tpf/controller/CiudadanoController.java
+++ TPF_Grupo10/src/main/java/ar/edu/unju/fi/tpf/controller/CiudadanoController.java
@@ -3,6 +3,7 @@ package ar.edu.unju.fi.tpf.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -18,7 +19,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class CiudadanoController {
 	
 	Logger logger = LoggerFactory.getLogger(CiudadanoController.class);
-	// Nomenclatura Trazas: logger.info("Method: NOMBREMETODO() - Information: DESCRIPCION SIMPLE");
-
+	
+	@RequestMapping("/inicio")
+	public String getInicioPage(Model model) {
+		return "ciudadano_inicio";
+	}
+	
+	@RequestMapping("/crear-cv")
+	public String getNuevoCVPage(Model model) {
+		return "ciudadano_crear_cv";
+	}
+	@RequestMapping("/ver-cv")
+	public String getCVPage(Model model) {
+		return "ciudadano_ver_cv";
+	}
+	@RequestMapping("/cursos")
+	public String getListaCursosPage(Model model) {
+		return "ciudadano_lista_cursos";
+	}
+	@RequestMapping("/ofertas")
+	public String getListaOfertasPage(Model model) {
+		return "ciudadano_lista_ofertas";
+	}
+	@RequestMapping("/salir")
+	public String getLogoutPage(Model model) {
+		return "redirect:/inicio";
+	}
 	
 }
